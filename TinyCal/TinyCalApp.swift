6a36870db1f633f980b7dceca9dd87ab4fbe9f1d
Alfred <brilychen@gmail.com>
2026-03-22 23:33:38 +0800

    Initial Commit

---
+++ TinyCal/TinyCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyCalApp.swift
+//  TinyCal
+//
+//  Created by Alfred Chan on 22/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct TinyCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
